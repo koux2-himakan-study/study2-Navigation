a0a4a4d9a6ede7ef092d604967aa0ffd3bc66110
koux2 <koux2.g@gmail.com>
2012-10-26 16:26:48 +0900

    Initial Commit

---
+++ Navigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Navigation
+//
+//  Created by Hideki Eguchi on 2012/10/26.
+//  Copyright (c) 2012年 Hideki Eguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Navigation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Navigation
+//
+//  Created by Hideki Eguchi on 2012/10/26.
+//  Copyright (c) 2012年 Hideki Eguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
